f9ea1728aa01b95cd7dcd7f9c5586b0d368722ff
XuDeHong <DeHongBoy@outlook.com>
2016-09-01 11:17:43 +0800

    fix some bug

--- CostList/SlideMenuViewController.m
+++ CostList/SlideMenuViewController.m
@@ -9,7 +9,9 @@
 #import "SlideMenuViewController.h"
 #import "ViewDeck/ViewDeck.h"
 
-@interface SlideMenuViewController ()
+@interface SlideMenuViewController () <IIViewDeckControllerDelegate>
+
+@property (nonatomic, strong) UIImageView *headerView;
 
 @end
 
@@ -23,7 +25,22 @@
     
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
     // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    //设置代理
+    self.viewDeckController.delegate = self;
+    
+    //设置HeaderView
+    self.headerView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, self.view.width, 240)];
+    self.headerView.contentMode = UIViewContentModeScaleAspectFill;
+    self.headerView.image = [UIImage imageNamed:@"sidebar_bg"];
+    self.headerView.clipsToBounds = YES;
+    self.tableView.tableHeaderView = self.headerView;
+    
+    [self enableTableViewScroll];
+}
 
+-(void)dealloc
+{
+    self.viewDeckController.delegate = nil;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -34,13 +51,40 @@
 -(void)viewDidAppear:(BOOL)animated
 {
     [super viewDidAppear:animated];
+    //侧栏打开时，状态栏为黑色
     [UIApplication sharedApplication].statusBarStyle = UIStatusBarStyleDefault;
 }
 
 -(void)viewDidDisappear:(BOOL)animated
 {
     [super viewDidDisappear:animated];
+    //侧栏关闭时，状态栏为白色
     [UIApplication sharedApplication].statusBarStyle = UIStatusBarStyleLightContent;
+}
+
+-(void)enableTableViewScroll
+{
+    //检测TableView内容是否超过屏幕，若超过，则可以滚动,否则禁止滚动
+    if(self.tableView.contentSize.height > SCREENHEIGHT || self.tableView.contentSize.height == SCREENHEIGHT)
+    {
+        self.tableView.scrollEnabled = YES;
+    }
+    else
+    {
+        self.tableView.scrollEnabled = NO;
+    }
+}
+
+#pragma mark - IIViewDeckControllerDelegate
+- (void)viewDeckController:(IIViewDeckController*)viewDeckController applyShadow:(CALayer*)shadowLayer withBounds:(CGRect)rect
+{
+    //默认的阴影效果，不过取消了阴影的动画
+    shadowLayer.masksToBounds = NO;
+    shadowLayer.shadowRadius = 10;
+    shadowLayer.shadowOpacity = 0.5;
+    shadowLayer.shadowColor = [[UIColor blackColor] CGColor];
+    shadowLayer.shadowOffset = CGSizeZero;
+    shadowLayer.shadowPath = [[UIBezierPath bezierPathWithRect:shadowLayer.bounds] CGPath];
 }
 
 @end
