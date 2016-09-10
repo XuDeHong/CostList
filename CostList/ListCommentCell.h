58fef630e54f17f69c201ee811fc3337a27590a6
XuDeHong <DeHongBoy@outlook.com>
2016-09-10 18:07:03 +0800

    add comment label to the cell

--- CostList/ListCommentCell.h
+++ CostList/ListCommentCell.h
@@ -1,17 +1,18 @@
 //
-//  ListCell.h
+//  ListCommentCell.h
 //  CostList
 //
-//  Created by 许德鸿 on 16/9/8.
+//  Created by 许德鸿 on 16/9/10.
 //  Copyright © 2016年 XuDeHong. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface ListCell : UITableViewCell
+@interface ListCommentCell : UITableViewCell
 
 @property (nonatomic,strong)IBOutlet UILabel *number;   //支出金额
 @property (nonatomic,strong)IBOutlet UILabel *title;    //标题
+@property (nonatomic,strong)IBOutlet UILabel *comment;  //备注
 @property (nonatomic,strong)IBOutlet UIImageView *imageIndicate; //图片标识
 
 @end
