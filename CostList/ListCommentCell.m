039e6ade1a510bbbfb97a921d1d57c50296276f5
XuDeHong <DeHongBoy@outlook.com>
2016-09-13 16:03:06 +0800

    update

--- CostList/ListCommentCell.m
+++ CostList/ListCommentCell.m
@@ -8,9 +8,6 @@
 
 #import "ListCommentCell.h"
 
-#define TableRowSeparatorColor [UIColor colorWithRed:216/255.0f green:216/255.0f blue:216/255.0f alpha:0.7]
-#define TableViewRowHeight 65
-
 @implementation ListCommentCell
 
 - (void)awakeFromNib {
@@ -22,15 +19,6 @@
     [super setSelected:selected animated:animated];
 
     // Configure the view for the selected state
-}
-
--(void)drawRect:(CGRect)rect
-{
-    //添加分割线
-    UIView *separator = [[UIView alloc] initWithFrame:CGRectMake(15,TableViewRowHeight - 1,SCREEN_WIDTH - 15, 1)];
-    separator.backgroundColor = TableRowSeparatorColor;
-    separator.tag = 500;    //做一个标记，方便获取
-    [self.contentView addSubview:separator];
 }
 
 @end
