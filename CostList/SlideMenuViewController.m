e489c57360484880963521b35dcfeb1b429431b8
许德鸿 <DeHongBoy@outlook.com>
2017-03-02 18:34:35 +0800

    APP can upload and download data from Server

--- CostList/SlideMenuViewController.m
+++ CostList/SlideMenuViewController.m
@@ -8,13 +8,12 @@
 
 #import "SlideMenuViewController.h"
 #import "SlideNavigationViewController.h"
+#import "GRRequestsManager.h"
 
-#define MYSERVER [NSString stringWithFormat:@"ftp://138.68.14.162/pub"]   //服务器保存数据位置
-
-
-@interface SlideMenuViewController ()
+@interface SlideMenuViewController () <GRRequestsManagerDelegate>
 
 @property (weak,nonatomic) SlideNavigationViewController *slideNavigationController;
+@property (nonatomic, strong) GRRequestsManager *requestsManager;   //FTP管理器
 
 @end
 
@@ -27,6 +26,10 @@
     
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
     // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    
+    //初始化FTP管理器
+    self.requestsManager = [[GRRequestsManager alloc] initWithHostname:@"138.68.14.162" user:@"chris" password:@"123456"];
+    self.requestsManager.delegate = self;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -39,14 +42,123 @@
 //从服务器下载数据到本地
 - (IBAction)downloadData
 {
+    //模拟器数据存入服务器的simulator文件夹
+    NSString *remoteDirectory = @"/simulator";
+    //真机数据存入服务器的real文件夹
+    //NSString *remoteDirectory = @"/real";
     
+    //获取服务器上保存的所有文件
+    [self.requestsManager addRequestForListDirectoryAtPath:remoteDirectory];
+
+    [self.requestsManager startProcessingRequests];
 }
 
 //从本地上传数据到服务器
 - (IBAction)uploadData
 {
-
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentsDirectory = [paths lastObject];
+    //模拟器数据存入服务器的simulator文件夹
+    NSString *remoteDirectory = @"/simulator";
+    //真机数据存入服务器的real文件夹
+    //NSString *remoteDirectory = @"/real";
+    
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    NSDirectoryEnumerator *dirEnum = [fileManager enumeratorAtPath:documentsDirectory];
+    NSString *fileName;
+    NSString *localPath;
+    NSString *remotePath;
+    //枚举Documents文件夹，将所有文件上传到服务器
+    while (fileName = [dirEnum nextObject])
+    {
+        localPath = [documentsDirectory stringByAppendingPathComponent:fileName];
+        remotePath = [remoteDirectory stringByAppendingPathComponent:fileName];
+        [self.requestsManager addRequestForUploadFileAtLocalPath:localPath toRemotePath:remotePath];
+    }
+    //以PhotoID为名在服务器创建一个空白文件夹
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    NSInteger photoId = [defaults integerForKey:@"PhotoID"];
+    NSString *idFileName = [NSString stringWithFormat:@"ID%ld",(long)photoId];
+    remotePath = [remoteDirectory stringByAppendingPathComponent:idFileName];
+    [self.requestsManager addRequestForCreateDirectoryAtPath:remotePath];
+    
+    [self.requestsManager startProcessingRequests];
 }
+
+#pragma mark - GRRequestsManagerDelegate
+
+- (void)requestsManager:(id<GRRequestsManagerProtocol>)requestsManager didScheduleRequest:(id<GRRequestProtocol>)request
+{
+    NSLog(@"requestsManager:didScheduleRequest:");
+}
+
+- (void)requestsManager:(id<GRRequestsManagerProtocol>)requestsManager didCompleteListingRequest:(id<GRRequestProtocol>)request listing:(NSArray *)listing
+{
+    NSLog(@"requestsManager:didCompleteListingRequest:listing: \n%@", listing);
+    
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentsDirectory = [paths lastObject];
+    //模拟器数据存入服务器的simulator文件夹
+    NSString *remoteDirectory = @"/simulator";
+    //真机数据存入服务器的real文件夹
+    //NSString *remoteDirectory = @"/real";
+    
+    //枚举服务器的文件名列表
+    for(NSString *fileName in listing)
+    {
+        if([fileName containsString:@"ID"])
+        {   //根据空白文件夹名更新用户配置的PhotoID
+            NSString *photoID = [fileName stringByReplacingOccurrencesOfString:@"ID" withString:@""];
+            NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+            [defaults setInteger:[photoID integerValue] forKey:@"PhotoID"];
+            [defaults synchronize];
+        }
+        else
+        {   //除空白文件夹外，其余文件都下载到Documents
+            NSString *localPath = [documentsDirectory stringByAppendingPathComponent:fileName];
+            NSString *remotePath = [remoteDirectory stringByAppendingPathComponent:fileName];
+            [self.requestsManager addRequestForDownloadFileAtRemotePath:remotePath toLocalPath:localPath];
+        }
+    }
+
+    [self.requestsManager startProcessingRequests];
+}
+
+- (void)requestsManager:(id<GRRequestsManagerProtocol>)requestsManager didCompleteCreateDirectoryRequest:(id<GRRequestProtocol>)request
+{
+    NSLog(@"requestsManager:didCompleteCreateDirectoryRequest:");
+}
+
+- (void)requestsManager:(id<GRRequestsManagerProtocol>)requestsManager didCompleteDeleteRequest:(id<GRRequestProtocol>)request
+{
+    NSLog(@"requestsManager:didCompleteDeleteRequest:");
+}
+
+- (void)requestsManager:(id<GRRequestsManagerProtocol>)requestsManager didCompletePercent:(float)percent forRequest:(id<GRRequestProtocol>)request
+{
+    NSLog(@"requestsManager:didCompletePercent:forRequest: %f", percent);
+}
+
+- (void)requestsManager:(id<GRRequestsManagerProtocol>)requestsManager didCompleteUploadRequest:(id<GRDataExchangeRequestProtocol>)request
+{
+    NSLog(@"requestsManager:didCompleteUploadRequest:");
+}
+
+- (void)requestsManager:(id<GRRequestsManagerProtocol>)requestsManager didCompleteDownloadRequest:(id<GRDataExchangeRequestProtocol>)request
+{
+    NSLog(@"requestsManager:didCompleteDownloadRequest:");
+}
+
+- (void)requestsManager:(id<GRRequestsManagerProtocol>)requestsManager didFailWritingFileAtPath:(NSString *)path forRequest:(id<GRDataExchangeRequestProtocol>)request error:(NSError *)error
+{
+    NSLog(@"requestsManager:didFailWritingFileAtPath:forRequest:error: \n %@", error);
+}
+
+- (void)requestsManager:(id<GRRequestsManagerProtocol>)requestsManager didFailRequest:(id<GRRequestProtocol>)request withError:(NSError *)error
+{
+    NSLog(@"requestsManager:didFailRequest:withError: \n %@", error);
+}
+
 
 #pragma mark -
 
